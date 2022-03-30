20d8efd978f978ecaf62a58aebdc1e8493e3f257
mitrofin <43621297+mitrofin@users.noreply.github.com>
2022-03-30 18:21:39 +0300

    start project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { Component } from 'react';
+import ContactForm from './components/ContactForm/ContactForm';
 
 export default class App extends Component {
   state = {
     contacts: [],
     name: '',
   };
+  handleSubmit = () => {};
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.handleSubmit} />
+        <h2>Contacts:</h2>
+      </div>
+    );
   }
 }
