f6a853869c7304d877267cb18d8045ec524077f4
mitrofin <43621297+mitrofin@users.noreply.github.com>
2022-03-31 16:16:49 +0300

    add contact form & contactlist

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import { Component } from 'react';
 import ContactForm from './components/ContactForm/ContactForm';
-
+import ContactList from './components/ContactList';
 export default class App extends Component {
   state = {
     contacts: [],
-    name: '',
+    filter: '',
   };
-  handleSubmit = () => {};
+  handleSubmit = contactObj => {
+    this.setState(prevState => {
+      return {
+        contacts: [...prevState.contacts, contactObj],
+      };
+    });
+  };
+
+  getFilteredContactsList = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase()),
+    );
+  };
   render() {
+    const { contacts, filter } = this.state;
+    const filteredContactsList = this.getFilteredContactsList();
     return (
       <div>
         <h1>Phonebook</h1>
         <ContactForm onSubmit={this.handleSubmit} />
         <h2>Contacts:</h2>
+        {filteredContactsList.length > 0 && (
+          <ContactList contacts={filteredContactsList} />
+        )}
       </div>
     );
   }
