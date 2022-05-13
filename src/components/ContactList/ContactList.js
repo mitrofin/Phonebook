19784862dfab8407b4b92c81f8190e529f48e46e
mitrofin <43621297+mitrofin@users.noreply.github.com>
2022-05-13 16:58:02 +0300

    Update ContactList.js

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,19 +1,41 @@
 import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import actions from '../../redux/contacts/contacts-action';
 import s from './ContactList.module.scss';
+import { getFilteredContacts } from '../../redux/contacts/contacts-selectors';
+const ContactList = () => {
+  const contacts = useSelector(getFilteredContacts);
+  /*   const contacts = useSelector(state => state.contacts.items);
+  const filter = useSelector(state => state.contacts.filter); */
+  const dispatch = useDispatch();
 
-export default function ContactList({ contacts, onDeleteButtonClick }) {
+  /* const getFilteredContactsList = (allcontacts, filter) => {
+    const normalizedFilter = filter.toLowerCase();
+    return allcontacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter),
+    );
+  }; */
+
+  /* const normalizedFilter = (allcontacts, filter) => {
+    return allcontacts.filter(({ name }) =>
+      name.toLowerCase().includes(filter.toLowerCase()),
+    );
+  }; */
+
+  /* const getFilteredContacts = getFilteredContactsList(contacts, filter); */
+
   return (
     <ul className={s.contactsList}>
-      {contacts.map((contact, idx) => (
-        <li key={contact.id} className={idx % 2 === 0 ? s.even : s.odd}>
+      {contacts.map(({ id, name, number }, idx) => (
+        <li key={id} className={idx % 2 === 0 ? s.even : s.odd}>
           <span>
-            {contact.name}: {contact.number}
+            {name}: {number}
           </span>
           <button
             type="button"
-            id={contact.id}
+            id={id}
             className={s.deleteButton}
-            onClick={e => onDeleteButtonClick(e.target.id)}
+            onClick={() => dispatch(actions.deleteContact(id))}
           >
             Delete
           </button>
@@ -21,8 +43,10 @@ export default function ContactList({ contacts, onDeleteButtonClick }) {
       ))}
     </ul>
   );
-}
+};
 ContactList.propTypes = PropTypes.shape({
   contacts: PropTypes.arrayOf(PropTypes.object).isRequired,
-  /* onDeleteButtonClick: PropTypes.func.isRequired, */
+  onDeleteButtonClick: PropTypes.func.isRequired,
 }).isRequired;
+
+export default ContactList;
