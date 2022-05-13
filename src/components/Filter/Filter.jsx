cc0eb1927525e4b8fed98dec6f95dcee29de2649
mitrofin <43621297+mitrofin@users.noreply.github.com>
2022-05-13 16:58:16 +0300

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,7 +1,14 @@
 import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import actions from '../../redux/contacts/contacts-action';
 import s from './Filter.module.scss';
 
-export default function Filter({ initialValue, onFilterChange }) {
+export default function Filter(/* { initialValue, onFilterChange } */) {
+  const initialValue = useSelector(state => state.contacts.filter);
+  const dispatch = useDispatch();
+
+  const onFilterChange = e => dispatch(actions.filterContact(e.target.value));
+
   return (
     <div className={s.inputWrapper}>
       <label className={s.filterLabel}>
@@ -11,9 +18,7 @@ export default function Filter({ initialValue, onFilterChange }) {
           type="text"
           name="name"
           value={initialValue}
-          onChange={e => {
-            onFilterChange(e.target.value);
-          }}
+          onChange={onFilterChange}
         />
       </label>
     </div>
