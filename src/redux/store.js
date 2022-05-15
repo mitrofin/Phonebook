5a9ad0ad7e43dec05d35a6ad04354493d452adf4
mitrofin <43621297+mitrofin@users.noreply.github.com>
2022-05-15 16:50:51 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,39 +1,12 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+
 import contactsReduser from './contacts/contacts-reduser';
 
-const persistConfig = {
-  key: 'contacts',
-  storage,
-  blacklist: ['filter'],
-};
-
-const middleware = [
-  ...getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  }),
-  logger,
-];
-
-const store = configureStore({
-  reducer: { contacts: persistReducer(persistConfig, contactsReduser) },
-  middleware,
+export const store = configureStore({
+  reducer: {
+    contacts: contactsReduser,
+  },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
   devTools: process.env.NODE_ENV === 'development',
 });
-
-const persistor = persistStore(store);
-
-export { store, persistor };
