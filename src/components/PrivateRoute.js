7fd5e70e93ced9cf35bf68d6a9f6266412e3f8bf
mitrofin <43621297+mitrofin@users.noreply.github.com>
2022-05-25 20:56:20 +0300

    Create PrivateRoute.js

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import { authSelectors } from '../redux/auth';
+
+export const PrivateRoute = ({ children }) => {
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+
+  return isLoggedIn ? children : <Navigate to="/login" />;
+};
