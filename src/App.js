944bfc8a3610964dfe7f9dd3bead78889cdaa73c
mitrofin <43621297+mitrofin@users.noreply.github.com>
2022-06-11 18:40:20 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,126 +1,96 @@
-/* import { useState } from 'react'; */
-import ContactForm from './components/ContactForm';
-import ContactList from './components/ContactList';
-import Filter from './components/Filter';
-import Section from './components/Section/section';
+import React, { lazy, useEffect, Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import Container from './components/Container/container';
-import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { contactsSelectors, contactsOperation } from './redux/contacts';
-import { toast, ToastContainer } from 'react-toastify';
+
+import { /* toast, */ ToastContainer } from 'react-toastify';
 import { TailSpin } from 'react-loader-spinner';
 import 'react-toastify/dist/ReactToastify.css';
+import { AppBar } from 'components/AppBar';
+import { authOperations, authSelectors } from 'redux/auth';
+import { PublicRoute } from 'components/PublicRoute';
+import { PrivateRoute } from 'components/PrivateRoute';
+
+import HomeView from 'views/HomeView/HomeView';
+import RegisterView from 'views/RegisterView/RegisterView';
+import LoginView from 'views/LoginView/LoginView';
+import ContactsView from 'views/ContactsView/ContactsView';
+/* const HomeView = lazy(() =>
+  import('./views/HomeView' ),
+);
+const RegisterView = lazy(() =>
+  import('./views/RegisterView' ),
+);
+const LoginView = lazy(() =>
+  import('./views/LoginView' ),
+);
+const ContactsView = lazy(() =>
+  import('./views/ContactsView' ),
+); */
 
 const App = () => {
   const dispatch = useDispatch();
-  const isLoading = useSelector(contactsSelectors.getIsLoading);
-  const isError = useSelector(contactsSelectors.getError);
-
-  useEffect(() => dispatch(contactsOperation.getContacts()), [dispatch]);
-
-  /* const [contacts, setContacts] = useState([]); */
-  /* const [filter, setFilter] = useState(''); */
-
-  /*   useEffect(() => {
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-    if (parsedContacts) {
-      setContacts(parsedContacts);
-    }
-  }, []);
+  const isFetchingCurrentUser = useSelector(authSelectors.getIsFetchingCurrent);
 
   useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-  }, [contacts]); */
-
-  /* const handleSubmit = ({ name, number }) => {
-    if (name.some({ name } === name)) {
-      return alert(`${name} already exists in your phonebook`);
-    }
-    setContacts(prevState => [...prevState, contacts]);
-  }; */
-
-  /*   const handleSubmit = ({ name, number, id }) => {
-    const contact = {
-      id,
-      name,
-      number,
-    };
-    if (contacts.some(contact => contact.name === name)) {
-      return alert(`${name} already exists in your phonebook`);
-    }
-    //setContacts(prevState => [...prevState, contact]);
-     //   const contact = {
-      //id,
-      //name,
-      //number,
-    //};
-    //const alreadyFind = contacts.find(contact => contact.name === name);
-
-    //alreadyFind
-      //? alert(`${name} is already in contacts.`)
-      //: setContacts(prevState => [...prevState], contact);
-  }; */
-
-  /* const handleDeleteContact = id => {
-    setContacts(contacts.filter(contact => contact.id !== id));
-  };
-
-  const handleFilterChange = filter => {
-    setFilter(filter);
-  };
-
-  const getFilteredContactsList = () => {
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase()),
-    );
-  }; */
-  /* const getFilteredContactsList = () => {
-    const normalizedFilter = filter.toLowerCase();
-
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter),
-    );
-  }; */
-
-  /* const filteredContactsList = getFilteredContactsList(); */
+    dispatch(authOperations.fetchCurrentUser());
+  }, [dispatch]);
 
   return (
     <Container>
-      {/* <h1 className={s.title}>Phonebook</h1> */}
-      <Section title="Phonebook">
-        <ContactForm /* onSubmit={handleSubmit} */ />
-        <Filter />
+      <AppBar />
 
-        <ToastContainer autoClose={1500} />
-        {isError && toast.error('We were unable to load contacts!')}
-      </Section>
-
-      {/* <h2 className={s.title}>Contacts:</h2> */}
-      <Section title="Saved contacts">
-        {isLoading && (
-          <TailSpin
-            heigth="150"
-            width="150"
-            color="#006280"
-            ariaLabel="loading"
-            y="150px"
-          />
-        )}
-        <ContactList />
-        <ToastContainer autoClose={1500} />
-      </Section>
-
-      {/* {getFilteredContactsList().length > 0 && (
-        <ContactList
-          contacts={getFilteredContactsList()}
-          onDeleteButtonClick={handleDeleteContact}
-        />
-      )} */}
-      {/* {contacts.length > 1 && (
-        <Filter initialValue={filter} onFilterChange={handleFilterChange} />
-      )} */}
+      {!isFetchingCurrentUser && (
+        <Suspense
+          fallback={
+            <TailSpin
+              heigth="120"
+              width="120"
+              color="#006280"
+              ariaLabel="loading"
+            />
+          }
+        >
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <PublicRoute restricted>
+                  <HomeView />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/register"
+              element={
+                <PublicRoute restricted redirectTo="/">
+                  <RegisterView />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <PublicRoute restricted>
+                  <LoginView />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path="/contacts"
+              element={
+                <PrivateRoute>
+                  <ContactsView />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </Suspense>
+      )}
+      <ToastContainer autoClose={1500} />
     </Container>
   );
 };
+
 export default App;
