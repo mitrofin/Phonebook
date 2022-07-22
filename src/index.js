ff4579e5cd5596024999d7ead8aee29fff055c17
mitrofin <43621297+mitrofin@users.noreply.github.com>
2022-07-22 18:57:55 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+/* import ReactDOM from 'react-dom'; */
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
 import { BrowserRouter } from 'react-router-dom';
-/* import ReactDOM from 'react-dom/client'; */
+
 import 'modern-normalize/modern-normalize.css';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
+/* ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
@@ -20,9 +21,28 @@ ReactDOM.render(
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
-);
-/* ReactDOM.createRoot(document.getElementById('root')).render(
+); */
+
+/* const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
 ); */
+ReactDOM.createRoot(document.getElementById('root')).render(
+  /* <React.StrictMode> */
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter basename="/contacts/">
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>,
+  /* </React.StrictMode> */
+);
