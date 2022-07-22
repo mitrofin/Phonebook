9c4973b86755a0bde7fef28e528d4d88cc801c76
mitrofin <43621297+mitrofin@users.noreply.github.com>
2022-07-22 18:58:19 +0300

    Update HomeView.js

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -1,28 +1,51 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Container } from '@material-ui/core';
-
+import { CSSTransition } from 'react-transition-group';
+/* import { Container } from '@material-ui/core'; */
+import { authSelectors } from 'redux/auth';
+import Modal from 'components/Modal/Modal';
+import Preloader from 'components/Preloader/Preloader';
 import phonebook from '../../images/phonebook.png';
 import styles from './HomeView.module.scss';
+import loginAndRegisterViewTransitionStyles from '../../components/transitionStyles/loginAndRegisterViewTransitionStyles.module.scss';
 
 const HomeView = () => {
+  const isFetchingCurrentUser = useSelector(authSelectors.getIsFetchingCurrent);
+
   return (
-    <div className={styles.Container}>
-      <img src={phonebook} alt="phonebook" height="600" width="310" />
+    <>
+      {isFetchingCurrentUser && (
+        <Modal>
+          <Preloader />
+        </Modal>
+      )}
 
-      <div className={styles.Wraper}>
-        <h1 className={styles.Title}>Phonebook</h1>
+      <CSSTransition
+        in={true}
+        appear
+        classNames={loginAndRegisterViewTransitionStyles}
+        timeout={500}
+        unmountOnExit
+      >
+        <div className={styles.Container}>
+          <img src={phonebook} alt="phonebook" height="600" width="310" />
 
-        <p className={styles.HomeText}>To start using our app, please</p>
-        <Link to="/login" className={styles.HomeLink}>
-          Log in
-        </Link>
-        <span className={styles.HomeSpan}>or</span>
-        <Link to="/register" className={styles.HomeLink}>
-          Register
-        </Link>
-      </div>
-    </div>
+          <div className={styles.Wraper}>
+            <h1 className={styles.Title}>Phonebook</h1>
+
+            <p className={styles.HomeText}>To start using our app, please</p>
+            <Link to="/login" className={styles.HomeLink}>
+              Log in
+            </Link>
+            <span className={styles.HomeSpan}>or</span>
+            <Link to="/register" className={styles.HomeLink}>
+              Register
+            </Link>
+          </div>
+        </div>
+      </CSSTransition>
+    </>
   );
 };
 
