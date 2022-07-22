448f703c7d076b5efff3865c45818c82722604d0
mitrofin <43621297+mitrofin@users.noreply.github.com>
2022-07-22 13:51:38 +0300

    Update AppBar.js

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -4,28 +4,41 @@ import { NavLink } from 'react-router-dom';
 
 import { UserMenu } from 'components/UserMenu';
 import { AuthNav } from 'components/AuthNav';
+
 import { authSelectors } from 'redux/auth';
 import styles from './AppBar.module.scss';
 import { Container } from '@mui/system';
+import { CSSTransition } from 'react-transition-group';
+import AppBarTransitionStyles from '../transitionStyles/appBarTransitionStyles.module.scss';
 
 export const AppBar = () => {
   const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
 
   return (
-    <Container maxWidth="xl">
-      <nav className={styles.authNav}>
-        <header className={styles.siteHeader}>
-          <NavLink
-            to="/"
-            className={styles.navLink}
-            /* activeClassName={styles.activeNavLink} */
-          >
-            {!isLoggedIn ? 'Home' : 'Contacts'}
-          </NavLink>
+    <>
+      <CSSTransition
+        in={true}
+        appear
+        classNames={AppBarTransitionStyles}
+        timeout={500}
+        unmountOnExit
+      >
+        <Container maxWidth="xl">
+          <nav className={styles.authNav}>
+            <header className={styles.siteHeader}>
+              <NavLink
+                to="/"
+                className={styles.navLink}
+                /* activeClassName={styles.activeNavLink} */
+              >
+                {!isLoggedIn ? 'Home' : 'Contacts'}
+              </NavLink>
 
-          {isLoggedIn ? <UserMenu /> : <AuthNav />}
-        </header>
-      </nav>
-    </Container>
+              {isLoggedIn ? <UserMenu /> : <AuthNav />}
+            </header>
+          </nav>
+        </Container>
+      </CSSTransition>
+    </>
   );
 };
