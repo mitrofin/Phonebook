ff46014b92e3800226a28c3c340793c25e133095
mitrofin <43621297+mitrofin@users.noreply.github.com>
2022-07-22 18:58:11 +0300

    Update ContactsView.js

--- src/views/ContactsView/ContactsView.js
+++ src/views/ContactsView/ContactsView.js
@@ -9,6 +9,9 @@ import Section from '../../components/Section/section';
 import ContactForm from 'components/ContactForm';
 import ContactList from 'components/ContactList';
 import Filter from 'components/Filter';
+import { CSSTransition } from 'react-transition-group';
+import loginAndRegisterViewTransitionStyles from '../../components/transitionStyles/loginAndRegisterViewTransitionStyles.module.scss';
+import contactViewTransitionStyles from '../../components/transitionStyles/contactViewTransitionStyles.module.scss';
 
 const ContactsView = () => {
   const dispatch = useDispatch();
@@ -22,22 +25,43 @@ const ContactsView = () => {
 
   return (
     <>
-      <Section title="Phonebook">
-        <ContactForm />
-        <Filter />
-      </Section>
+      <CSSTransition
+        in={true}
+        appear
+        classNames={loginAndRegisterViewTransitionStyles}
+        timeout={500}
+        unmountOnExit
+      >
+        <Section title="Phonebook">
+          <ContactForm />
+          <Filter />
+        </Section>
+      </CSSTransition>
 
-      <Section title="Saved contacts">
-        {isLoading && (
-          <Grid heigth="120" width="120" color="#5773f6" ariaLabel="loading" />
-        )}
-        {isContacts.length === 0 ? (
-          <h3>You don't have contacts yet</h3>
-        ) : (
-          <ContactList />
-        )}
-        {isError && toast.error('We were unable to load contacts!')}
-      </Section>
+      <CSSTransition
+        in={true}
+        appear
+        classNames={contactViewTransitionStyles}
+        timeout={500}
+        unmountOnExit
+      >
+        <Section title="Saved contacts">
+          {isLoading && (
+            <Grid
+              heigth="120"
+              width="120"
+              color="#5773f6"
+              ariaLabel="loading"
+            />
+          )}
+          {isContacts.length === 0 ? (
+            <h3>You don't have contacts yet</h3>
+          ) : (
+            <ContactList />
+          )}
+          {isError && toast.error('We were unable to load contacts!')}
+        </Section>
+      </CSSTransition>
       <ToastContainer autoClose={1500} />
     </>
   );
