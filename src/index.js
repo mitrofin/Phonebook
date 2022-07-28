56d57c01b947839773a2ed2d7a9f1b4588baf60e
mitrofin <43621297+mitrofin@users.noreply.github.com>
2022-07-28 15:27:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -23,26 +23,26 @@ import './index.css';
   document.getElementById('root'),
 ); */
 
-/* const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="/">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+);
+/* ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/">
           <App />
         </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>,
 ); */
-ReactDOM.createRoot(document.getElementById('root')).render(
-  /* <React.StrictMode> */
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter basename="/contacts/">
-        <App />
-      </BrowserRouter>
-    </PersistGate>
-  </Provider>,
-  /* </React.StrictMode> */
-);
